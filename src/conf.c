f0158c3600f8680859e29b5d2251c4c246fb94b2
Oleg Gordienko <admin@gordio.pp.ua>
2012-11-26 22:09:24 +0200

    Реализация настроек

--- src/conf.c
+++ src/conf.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
+#include <glib.h>
+#include <stdio.h>
+#include <json.h>
+#include "log.h"
 
-#include "utils.h"
 #include "conf.h"
 
 // Должен быть минимум 1 элемент
@@ -9,8 +12,8 @@ Conf CONF_DEF[] = {
 	{"window.save_position", "1", NULL},
 	{"window.width", "1004", NULL}, // MIN_WINDOW_WIDTH
 	{"window.height", "650", NULL},
-	{"flash.fullscreen_size", "600", NULL},
-	{"flash.windowed_size", "200", NULL},
+	{"flash.fullscreen_height", "600", NULL},
+	{"flash.windowed_height", "200", NULL},
 };
 
 
@@ -22,7 +25,6 @@ conf_init(void)
 {
 	Conf *cur = &CONF;
 
-	// init first element
 	unsigned int conf_def_length = sizeof(CONF_DEF)/sizeof(Conf);
 
 	// перебираем все стандартные значения заполняя глобальный CONF
@@ -30,6 +32,7 @@ conf_init(void)
 		// заполняем значения
 		cur->name = g_strdup(CONF_DEF[i].name);
 		cur->value = g_strdup(CONF_DEF[i].value);
+		vlog("init: %s = %s", cur->name, cur->value);
 
 		if (i+1 < conf_def_length) {
 			// создаем очередной элемент
@@ -39,51 +42,132 @@ conf_init(void)
 		}
 	}
 	cur->next = NULL;
+	
+	conf_load();
 
 	return true;
 }
 
 bool
-conf_get(const char const *name, char *value)
+conf_get(const char const *name, char **value)
 {
 	Conf *cur = &CONF;
 
 	while (cur) {
 		if (strcmp(name, cur->name) == 0) {
-			printf("%s\n", cur->value);
+			*value = cur->value;
+			vlog("get: %s = %s", name, cur->value);
 			return true;
 		}
 		cur = cur->next;
 	}
 
-	// Can't find
+	vlog("get: %s = <not find>", name);
 	return false;
 }
 
 bool
-conf_set(const char const *name, char *value)
+conf_get_int(const char const *name, int *value)
 {
+	char *str = NULL;
 
+	if (conf_get(name, &str)) {
+		if (str) {
+			*value = atoi(str);
+			return true;
+		}
+	}
 
-	// Error
 	return false;
 }
 
 bool
-conf_save(const char const *file_name)
+conf_set(const char const *name, const char *value)
 {
+	Conf *cur = &CONF;
 
-	// TODO: Save config to file
+	while (cur) {
+		if (strcmp(name, cur->name) == 0) {
+			// освобождаем старую память
+			if (cur->value) {
+				free(cur->value);
+			}
+
+			// записываем новое значение
+			cur->value = g_strdup(value);
+			vlog("set: %s = %s", cur->name, cur->value);
+			return true;
+		}
+		cur = cur->next;
+	}
+
+	// Создаем новый элемент
+	cur = (Conf *)malloc(sizeof(Conf));
+	cur->name = g_strdup(name);
+	cur->value = g_strdup(value);
+	cur->next = NULL;
+
+	vlog("set: (new)%s = %s", cur->name, cur->value);
+	return false;
+}
+
+bool
+conf_set_int(const char const *name, int value)
+{
+	int size = snprintf(NULL, 0, "%i", value) + 1;
+	char *buf = malloc(size);
+	snprintf(buf, size, "%i", value);
+
+	bool status;
+	status = conf_set(name, buf);
+
+	free(buf);
+
+	return status;
+}
+
+bool
+conf_save()
+{
+	json_object *root;
+
+	root = json_object_new_object();
+	Conf *cur = &CONF; // глобальный конфиг
+	unsigned int conf_def_length = sizeof(CONF_DEF)/sizeof(Conf);
+
+	while (cur) {
+		// сохраняем только не совпадающие с дефолтом значения
+		for (unsigned int i = 0; i < conf_def_length; i++) {
+			if (strcmp(cur->name, CONF_DEF[i].name) == 0 &&
+					strcmp(cur->value, CONF_DEF[i].value) != 0) {
+				json_object_object_add(root, cur->name, json_object_new_string(cur->value));
+				continue;
+			}
+		}
+		cur = cur->next;
+	}
+
+	json_object_to_file(CONF_FILE_NAME, root);
+	json_object_put(root);
 
 	return false;
 }
 
 bool
-conf_load(const char const *file_name)
+conf_load()
 {
+	json_object *root;
 
-	// TODO: Load config from file
+	if (!g_file_test(CONF_FILE_NAME, G_FILE_TEST_EXISTS)) {
+		return false;
+	}
 
-	return false;
+	root = json_object_from_file(CONF_FILE_NAME);
+
+	json_object_object_foreach(root, key, val) {
+		conf_set(key, json_object_get_string(val));
+	}
+
+	return true;
 }
 /* vim: set fdm=marker ts=4 sw=4 tw=100 fo-=t ff=unix: */
--- src/conf.h
+++ src/conf.h
@@ -3,6 +3,8 @@
 
 #include <stdbool.h>
 
+#define CONF_FILE_NAME "config.json"
+
 
 typedef struct {
 	char *name;
@@ -13,7 +15,18 @@ typedef struct {
 
 
 bool conf_init(void);
-bool conf_get(const char const *name, char *value);
+
+bool conf_get(const char const *name, char **value);
+bool conf_get_int(const char const *name, int *value);
+#define conf_get_char(a, b) conf_get(a,b)
+
+bool conf_set(const char const *name, const char *value);
+bool conf_set_int(const char const *name, int value);
+#define conf_set_char(a, b) conf_set(a,b)
+
+bool conf_save(void);
+bool conf_load(void);
+
 
 
 #endif
